92ac2672cd0f7119482f4fff3de44deb8188d997
stringiest <lucyjstringer@gmail.com>
2021-01-09 16:55:41 +0000

    added bcrypt to gemfile, then created user, list & gift tables in postgres db

---
+++ db/migrate/20210109165201_create_gifts.rb
@@ -0,0 +1,15 @@
+class CreateGifts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :gifts do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :list, null: false, foreign_key: true
+      t.string :name
+      t.string :url
+      t.string :notes
+      t.boolean :bought
+      t.string :bought_by
+
+      t.timestamps
+    end
+  end
+end
