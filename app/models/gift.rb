003ede1e6ba658a9f3669a01915d5e22d06b3609
stringiest <lucyjstringer@gmail.com>
2021-01-12 10:34:56 +0000

    added lists controller & associated tests, ran rubocop & corrected multiple errors

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Gift < ApplicationRecord
   belongs_to :user
   belongs_to :list
---
+++ spec/features/lists_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'lists' do
+  scenario 'user can sign in and make a list' do
+    login_and_make_list
+
+    expect(current_path).to eq('/lists')
+    expect(page).to have_content('2021 Christmas List')
+  end
+
+  scenario 'user can edit the name of a list' do
+    login_and_make_list
+    click_link('Edit')
+    fill_in('name', with: '')
+    fill_in('name', with: '2021 Birthday list')
+
+    click_button('Submit')
+
+    expect(current_path).to eq('/lists')
+    expect(page).to have_content('2021 Birthday list')
+    expect(page).to have_content('2021-12-25')
+  end
+
+  scenario 'user can edit the event date of a list' do
+    login_and_make_list
+    click_link('Edit')
+    fill_in('list_event_date', with: '')
+    fill_in('list_event_date', with: '04/07/2021')
+
+    click_button('Submit')
+
+    expect(current_path).to eq('/lists')
+    expect(page).to have_content('2021 Christmas List')
+    expect(page).to have_content('2021-07-04')
+  end
+
+  scenario 'user can delete a list' do
+    login_and_make_list
+    click_link('Delete')
+    click_link('ok')
+
+    expect(current_path).to eq('/lists')
+    expect(page).not_to have_content('2021 Christmas List')
+  end
+
+  scenario 'user cannot add list if not signed in' do
+    visit('/lists')
+
+    expect(page).to have_no_link('New List')
+  end
+end
